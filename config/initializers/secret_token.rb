486d802478ee914339c26d129e11a1197e5c1e69
teejdxgt <tj.lindsley@gmail.com>
2011-12-20 11:41:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRealWeddingApp::Application.config.secret_token = 'd4f6bb7f14f393c5db4da89aac9383fecc36e6eca3478be2be61c718da646de8b06923a941f367ebb24028f223e5ddbabc833d0552121cc56e61fe6d1598071a'
