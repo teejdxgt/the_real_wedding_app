616f0fc164882f781388ba24868a22b5f32a5275
teejdxgt <tj.lindsley@gmail.com>
2012-11-15 16:53:51 -0500

    Numbers can't go below 0 anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
-  validates :number_of_guests, numericality: {only_integer: true, less_than_or_equal_to: 1}                  
+  validates :number_of_guests, numericality: {only_integer: true, less_than_or_equal_to: 1, greater_than_or_equal_to: 0}                  
   before_save :encrypt_password
   
   scope :admin, where(:admin => true)
